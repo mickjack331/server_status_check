7521d17d271d65bf7ea3d28fc900f4d2eb2b4057
mickjack331 <ds.201904250928@gmail.com>
2019-05-07 13:27:15 +0900

    Modified server_check.py

--- server_check.py
+++ server_check.py
@@ -6,13 +6,4 @@ print('Loading function... ')
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-# set your access key here, don't use my key!!
-AWS_ACCESS_KEY = 'AKIAJTS52TFCQHJFOI3A'
-# set your secret access key here, don't use my key!!
-AWS_ACCESS_SECRET = '9Z/jzL0J+haty7f2hwrkK0BDmw06wq/EcnNgscEk'
-
-# option
-AWS_REGION = 'ap-northeast-1'
-EC2_TARGET_NAME_TAG = 'CheckInstanceStatus'
-
 def handler(event, context):
